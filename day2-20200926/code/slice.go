0e7c84a445d4446537e258032a0df99f71c91aa5
wangqing <wsqnihao@126.com>
2020-10-21 22:11:04 +0800

    change 10212211

--- day2-20200926/code/slice.go
+++ day2-20200926/code/slice.go
@@ -5,55 +5,78 @@ import "fmt"
 
 func main() {
 	// 定义切片
-	var names []string         // 这个是nil； nil和空切片并不是一样的
-	fmt.Printf("%T\n", names)  // []string
-	fmt.Printf("%#v\n", names) // []string(nil)
-	fmt.Printf("%q\n", names)  // []
-	fmt.Println(names)         // []
+	var names []string // 这个是nil； nil和空切片并不是一样的
+	// fmt.Printf("%T\n", names)  // []string
+	// fmt.Printf("%#v\n", names) // []string(nil)
+	// fmt.Printf("%q\n", names)  // []
+	// fmt.Println(names)         // []
 
 	names = []string{"abc", "sen"}
-	fmt.Printf("%T\n", names)
-	fmt.Printf("%#v\n", names)
+	// fmt.Printf("%T\n", names)
+	// fmt.Printf("%#v\n", names)
 
 	// 字面量
 	// []type{}  这个是空切片
-	// []
+	// []type{v1, v2, v3, ... vn}  值的方式
+	// []type{i1:v1, i2:v2 ... in:vn}
 
 	names = []string{1: "abc", 10: "sen"}
-	fmt.Printf("%T\n", names) // []string
-	fmt.Printf("%q\n", names) // ["" "abc" "" "" "" "" "" "" "" "" "sen"]
+	// fmt.Printf("%T\n", names) // []string
+	// fmt.Printf("%q\n", names) // ["" "abc" "" "" "" "" "" "" "" "" "sen"]
 
 	// 访问 修改元素
 	// 索引
 	// *  超过范围的话
-	fmt.Println(names[1])
-	fmt.Println(names[10])
+	// fmt.Println(names[1])  // abc
+	// fmt.Println(names[10]) // sen
 
 	// 长度
-	fmt.Println(len(names)) // 11
+	// fmt.Println(len(names)) // 11
 
-	// 遍历
-	for i := 0; i < len(names); i++ {
-		fmt.Println(i, names[i])
-	}
+	// 遍历  下面3种不同的遍历方式，得到结果一样
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(i, names[i])  // 索引i 和value值
+	// }
 
-	for  v1 := range names {
-		fmt.Println(v1 names[v1])
-	}
+	// for v1 := range names {
+	// 	fmt.Println(v1, names[v1])  // v1 得到的是索引值 一个参数的事
+	// }
 
-	for i, v := range names {
-		fmt.Println(i, v)
-	}
+	// for i, v := range names {  //最直接的写法
+	// 	fmt.Println(i, v)
+	// }
 
+	// 添加元素
+	// 切片的末尾
+	names = append(names, "liuran")
+	// fmt.Printf("%q, %#v\n", names, len(names))  //["" "abc" "" "" "" "" "" "" "" "" "sen" "liuran"], 12
 
-	// 
-	names =  append
+	// 删除元素  通过切片操作实现
+	//names[start:end] names 中从start 开始到end -1 所有元素组成的切片 不包含end
+	// names[1:10]
+	//  1   2                              11    12
+	// ["" "abc" "" "" "" "" "" "" "" "" "sen" "liuran"]
+	// fmt.Printf("%q\n", names[1:10]) //["abc" "" "" "" "" "" "" "" ""]  //这样就去掉了后面2个
+	// names = names[1:len(names)]
+	// fmt.Printf("%q\n", names) //["abc" "" "" "" "" "" "" "" "" "sen" "liuran"] 没有去掉
 
-	// 删除元素  切片操作
-	names[start:end] names 中从start 开始到end -1 所有元素组成的切片 不包含end
-	
+	// names = names[0 : len(names)-1] //
+	// fmt.Printf("%q\n", names)       //["abc" "" "" "" "" "" "" "" "" "sen"]  这样就去掉了后面一个
 
+	//删除中间的元素
+	//目标是删除索引为4的元素  用copy函数实现  copy(dst, src) src -> dst
+	nums := []int{0, 1, 2, 3, 4, 5}
 
+	nums2 := []int{10, 11, 12, 13, 14, 15, 16}
 
+	copy(nums, nums2)
+
+	fmt.Println(nums, nums2) //[10 11 12 13 14 15] [10 11 12 13 14 15 16]
+
+	copy(nums[3:len(nums)], nums[4:len(nums)])
+	// [13 14 15]   [14 15]
+
+	fmt.Println(nums[0:len(nums)])
+	fmt.Println(nums[0 : len(nums)-1])
 
 }
--- day3-20201008/codes/5-typev1.go
+++ day3-20201008/codes/5-typev1.go
@@ -13,15 +13,16 @@ func mul(a, b int) int {
 
 func main() {
 	var f func(int, int) int = add //定义一个变量f  类型是函数 值是add
-	var fs []func(int, int) int    // 定义一个变量fs 类型是切片中的函数， 一般不会这么用，一般会自定义类型。 后续使用追踪分析一下
+	var fs []func(int, int) int    // 定义一个变量fs 类型是切片(但是类型是函数)， 一般不会这么用，一般会自定义类型。 后续使用追踪分析一下
 
-	fs = append(fs, add, mul)
+	fs = append(fs, add, mul) //这个是切片的一种写法  给fs 这个切片添加两个元素
 
 	c := f(2, 3)
 	fmt.Println(c) //5
 
 	for _, v := range fs {
-		fmt.Println(v(2, 3)) // 5 6
+		fmt.Printf("%T, %#v\n", v, v)
+		// fmt.Println(v(2, 3)) // 5 6
 	}
 
 }
